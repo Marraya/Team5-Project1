fbe734685d128ab7c5b2b9d641a8d372e5891a94
Mario Arraya <mario@imenutogo.com>
2019-03-04 20:39:59 -0500

    Map worksgit add .

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,6 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
+// $("#map-search").on("click",function(){
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+//     var query = parseInt($("#term-search").val().trim());
+//     console.log(query)
+
+// });
